d55883354ed729aace6acf1cb5f5244d1332f786
Zulham Syafrawi <official.zulham@gmail.com>
2022-06-24 11:36:42 +0700

    add to belum dibaca done

--- dist/script.js
+++ dist/script.js
@@ -38,11 +38,16 @@ function renderUI(storage) {
   if (storage.length) {
     // Render UI
     const belumContainer = document.getElementById("belum-card-container");
+    const sudahContainer = document.getElementById("sudah-card-container");
     belumContainer.innerHTML = "";
+    sudahContainer.innerHTML = "";
     storage.forEach((book) => {
       if (!book.isComplete) {
         const card = addToBelumDibaca(book);
         belumContainer.appendChild(card);
+      } else if (book.isComplete) {
+        const card = addToSudahDibaca(book);
+        sudahContainer.appendChild(card);
       }
     });
   }
@@ -106,5 +111,42 @@ function addToBelumDibaca(data) {
         </svg></button>
     </div>
   `;
+  return result;
+}
+
+// Sudah Card
+function addToSudahDibaca(data) {
+  const result = document.createElement("div");
+  result.setAttribute("data-id", data.id);
+  result.className =
+    "border border-slate-200/90 py-2 px-3 rounded flex justify-between items-center";
+  result.innerHTML = `
+  <!-- Book Detail -->
+  <div>
+    <!-- Book Title -->
+    <p class="font-semibold">${data.judul}</p>
+    <!-- Author -->
+    <p class="text-sm text-slate-600 font-medium">${data.penulis}</p>
+    <!-- Year -->
+    <p class="text-sm text-slate-500">${data.tahun}</p>
+  </div>
+  <!-- Book Action -->
+  <div class="flex gap-3">
+    <!-- Delete -->
+    <button class="p-2 group" title="Hapus buku"><svg xmlns="http://www.w3.org/2000/svg"
+        class="h-5 w-5 fill-slate-300 group-hover:fill-red-600" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd"
+          d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+          clip-rule="evenodd" />
+      </svg></button>
+    <button class="p-2 group rounded-full hover:bg-slate-500" title="Tandai belum dibaca">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 fill-slate-600 group-hover:fill-white"
+        viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd"
+          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+          clip-rule="evenodd" />
+      </svg></button>
+  </div>
+    `;
   return result;
 }
