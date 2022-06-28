e7308ee34c55841b59624ef55e5d370dfb8b6dfe
Zulham Syafrawi <official.zulham@gmail.com>
2022-06-28 22:09:53 +0700

    search function works

--- dist/script.js
+++ dist/script.js
@@ -93,12 +93,63 @@ const searchField = document.getElementById("search");
 const searchBtn = document.getElementById("search-btn");
 
 searchBtn.addEventListener("click", function (e) {
+  const searchContainer = document.getElementById("search-container");
+
   if (searchField.value != "") {
     const storage = JSON.parse(localStorage.getItem(STORAGE_KEY));
     const pattern = new RegExp(searchField.value, "i");
     const result = storage.filter((item) => {
       return pattern.test(item.title);
     });
-    console.log(result);
+
+    if (result.length) {
+      searchContainer.innerHTML = "";
+      result.forEach((item) => {
+        // console.log(item);
+        const card = searchResultCard(item);
+        searchContainer.appendChild(card);
+      });
+    } else {
+      searchContainer.innerHTML =
+        "Tidak ada hasil ditemukan untuk " + searchField.value;
+    }
+  } else {
+    searchContainer.innerHTML = "";
   }
 });
+
+function searchResultCard(data) {
+  const result = document.createElement("div");
+  result.className =
+    "shadow py-2 px-3 rounded flex justify-between items-center";
+  let pillCard;
+  if (data.isComplete) {
+    pillCard = `
+    <div class="text-sm font-semibold bg-emerald-100 text-emerald-500 px-4 py-2 rounded-full">
+      Sudah dibaca
+    </div>
+    `;
+  } else {
+    pillCard = `
+    <div class="text-sm font-semibold bg-slate-200 text-slate-500 px-4 py-2 rounded-full">
+      Belum dibaca
+    </div>
+    `;
+  }
+
+  result.innerHTML = `
+  <!-- Book Detail -->
+  <div>
+    <!-- Book Title -->
+    <p class="font-semibold">${data.title}</p>
+    <!-- Author -->
+    <p class="text-sm text-slate-600 font-medium">${data.author}</p>
+    <!-- Year -->
+    <p class="text-sm text-slate-500">${data.year}</p>
+  </div>
+  <!-- Book Info -->
+  ${pillCard}
+  `;
+
+  return result;
+}
